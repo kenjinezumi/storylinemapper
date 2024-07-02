254492634d92608b564a01338e3133f586dd94a7
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-07-02 13:10:30 +0100

    Improve the edges

--- storylinemapper/d3/script1.js
+++ storylinemapper/d3/script1.js
@@ -1,94 +1,170 @@
-var data = {json_data};
+const data = {json_data};
+const width = {width};
+const height = {height};
+const showActions = {show_actions};
 
-var width = 960,
-    height = 600;
+console.log("Data:", data);
+console.log("Width:", width);
+console.log("Height:", height);
+console.log("Show Actions:", showActions);
 
-var svg = d3.select("body").append("svg")
+// Create SVG
+const svg = d3.select("body")
+    .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("preserveAspectRatio", "xMidYMid meet");
 
-var simulation = d3.forceSimulation(data.nodes)
-    .force("link", d3.forceLink(data.links).id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody().strength(-200))
-    .force("center", d3.forceCenter(width / 2, height / 2));
+console.log("SVG element created");
 
-var link = svg.append("g")
+// Add a marker for arrowheads
+svg.append("defs").append("marker")
+    .attr("id", "arrowhead")
+    .attr("viewBox", "-0 -5 10 10")
+    .attr("refX", 13)
+    .attr("refY", 0)
+    .attr("orient", "auto")
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("xoverflow", "visible")
+    .append("svg:path")
+    .attr("d", "M 0,-5 L 10 ,0 L 0,5")
+    .attr("fill", "#999")
+    .style("stroke","none");
+
+console.log("Arrowhead marker added");
+
+// Simulation
+const simulation = d3.forceSimulation(data.nodes)
+    .force("link", d3.forceLink(data.links).id(d => d.id).distance(200))
+    .force("charge", d3.forceManyBody().strength(-300))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("collision", d3.forceCollide().radius(50));
+
+console.log("Simulation created");
+
+// Links
+const link = svg.append("g")
     .attr("class", "links")
     .selectAll("line")
     .data(data.links)
-    .enter().append("line")
-    .attr("class", "link")
-    .on("click", function(event, d) {
-        d3.selectAll(".link").style("stroke", "#999");
-        d3.select(this).style("stroke", "#d62728");
-        showTooltip(d, event.pageX, event.pageY, "Actions: " + d.actions);
-    })
-    .on("mouseover", function(event, d) {
-        showTooltip(d, event.pageX, event.pageY, "Actions: " + d.actions);
-    })
-    .on("mouseout", function(d) {
-        hideTooltip();
-    });
+    .enter()
+    .append("line")
+    .attr("stroke-width", 2)
+    .attr("marker-end", "url(#arrowhead)")
+    .style("stroke", "#999");
 
-var node = svg.append("g")
+console.log("Links added");
+
+// Nodes
+const node = svg.append("g")
     .attr("class", "nodes")
     .selectAll("circle")
     .data(data.nodes)
-    .enter().append("circle")
-    .attr("class", "node")
-    .attr("r", 5)
+    .enter()
+    .append("circle")
+    .attr("r", 10)
+    .attr("fill", "#69b3a2")
     .call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
-        .on("end", dragended))
-    .on("click", function(event, d) {
-        d3.selectAll(".node").style("stroke", "#fff");
-        d3.select(this).style("stroke", "#d62728");
-        showTooltip(d, event.pageX, event.pageY, "Node: " + d.id);
-    })
-    .on("mouseover", function(event, d) {
-        showTooltip(d, event.pageX, event.pageY, "Node: " + d.id);
-    })
-    .on("mouseout", function(d) {
-        hideTooltip();
-    });
+        .on("end", dragended));
 
-var labels = svg.append("g")
+console.log("Nodes added");
+
+// Labels
+const label = svg.append("g")
     .attr("class", "labels")
     .selectAll("text")
     .data(data.nodes)
-    .enter().append("text")
-    .attr("class", "label")
-    .text(function(d) { return d.id; });
+    .enter()
+    .append("text")
+    .attr("dy", 3)
+    .attr("dx", 12)
+    .text(d => d.id)
+    .style("pointer-events", "none");
 
-if ({show_actions}) {
-    link.append("title")
-        .text(function(d) { return d.actions; });
+console.log("Labels added");
+
+// Tooltips
+const tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+    .style("position", "absolute")
+    .style("background", "lightsteelblue")
+    .style("border", "0px")
+    .style("border-radius", "8px")
+    .style("padding", "8px")
+    .style("pointer-events", "none");
+
+console.log("Tooltip div created");
+
+node.on("mouseover", (event, d) => {
+        tooltip
+            .style("opacity", 1)
+            .html(d.id)
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 10) + "px");
+    })
+    .on("mouseout", () => {
+        tooltip.style("opacity", 0);
+    });
+
+link.on("mouseover", (event, d) => {
+        tooltip
+            .style("opacity", 1)
+            .html(d.actions)
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 10) + "px");
+    })
+    .on("mouseout", () => {
+        tooltip.style("opacity", 0);
+    });
+
+console.log("Tooltip events added");
+
+// Add text to links if showActions is true
+if (showActions) {
+    svg.append("g")
+        .attr("class", "link-labels")
+        .selectAll("text")
+        .data(data.links)
+        .enter()
+        .append("text")
+        .attr("class", "action-label")
+        .attr("text-anchor", "middle")
+        .text(d => d.actions);
+    console.log("Action labels added");
 }
 
-simulation
-    .nodes(data.nodes)
-    .on("tick", ticked);
-
-simulation.force("link")
-    .links(data.links);
-
-function ticked() {
+// Simulation tick
+simulation.on("tick", () => {
     link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
     node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
 
-    labels
-        .attr("x", function(d) { return d.x; })
-        .attr("y", function(d) { return d.y; });
-}
+    label
+        .attr("x", d => d.x)
+        .attr("y", d => d.y);
 
+    if (showActions) {
+        svg.selectAll(".action-label")
+            .attr("x", d => (d.source.x + d.target.x) / 2)
+            .attr("y", d => (d.source.y + d.target.y) / 2);
+    }
+});
+
+console.log("Simulation tick added");
+
+// Drag functions
 function dragstarted(event, d) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
@@ -106,25 +182,4 @@ function dragended(event, d) {
     d.fy = null;
 }
 
-function showTooltip(d, x, y, text) {
-    var tooltip = d3.select(".tooltip");
-    if (tooltip.empty()) {
-        tooltip = d3.select("body").append("div").attr("class", "tooltip");
-    }
-    tooltip.style("left", x + "px")
-           .style("top", y + "px")
-           .style("opacity", 1)
-           .html(text);
-}
-
-function hideTooltip() {
-    d3.select(".tooltip").style("opacity", 0);
-}
-
-d3.select("body").on("click", function(event) {
-    if (event.target.tagName !== "circle" && event.target.tagName !== "line") {
-        d3.selectAll(".link").style("stroke", "#999");
-        d3.selectAll(".node").style("stroke", "#fff");
-        hideTooltip();
-    }
-});
+console.log("Drag functions added");
--- storylinemapper/network.py
+++ storylinemapper/network.py
@@ -40,11 +40,11 @@ def load_css(style: str) -> str:
     with open(path, "r") as file:
         return file.read()
 
-def load_js(script: str, json_data: str, show_actions: bool) -> str:
+def load_js(script: str, json_data: str, show_actions: bool, width: int, height: int) -> str:
     path = os.path.join(os.path.dirname(__file__), "d3", script + ".js")
     with open(path, "r") as file:
         js_code = file.read()
-    return js_code.replace("{json_data}", json_data).replace("{show_actions}", str(show_actions).lower())
+    return js_code.replace("{json_data}", json_data).replace("{show_actions}", str(show_actions).lower()).replace("{width}", str(width)).replace("{height}", str(height))
 
 def generate_network_iframe(G: nx.Graph, title: str = "Entity Relation Network", style: str = "style1", script: str = "script1", show_actions: bool = False, width: str = "960px", height: str = "600px") -> str:
     nodes = [{"id": node} for node in G.nodes()]
@@ -57,7 +57,7 @@ def generate_network_iframe(G: nx.Graph, title: str = "Entity Relation Network",
 
     json_data = json.dumps(data)
     css_content = load_css(style)
-    js_content = load_js(script, json_data, show_actions)
+    js_content = load_js(script, json_data, show_actions, int(width[:-2]), int(height[:-2]))
 
     html_template = f"""
 <!DOCTYPE html>
@@ -72,10 +72,13 @@ def generate_network_iframe(G: nx.Graph, title: str = "Entity Relation Network",
     <div style="width: {width}; height: {height};">
         <script src="https://d3js.org/d3.v6.min.js"></script>
         <script>
+            console.log('Data: {json_data}');
+            console.log('Show Actions: {show_actions}');
             {js_content}
         </script>
     </div>
 </body>
 </html>
     """
+    print("Generated HTML:", html_template)  # Add this line for debugging
     return html_template
