df61e4a91a95cf85f6442ca43ee74e6c9e1f713b
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-07-08 18:51:59 +0100

    Refactor the html in a different file

--- storylinemapper/__init__.py
+++
@@ -1,4 +0,0 @@
-# storylinemapper/__init__.py
-
-from .network import extract_entities, extract_relations, generate_network, generate_network_iframe
-from .timeline import extract_events,  generate_timeline_iframe
